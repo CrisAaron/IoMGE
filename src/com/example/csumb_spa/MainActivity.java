8f8ff1a0b0a8a4fcbe336e421883ce1146d5abb8
CrisAaron <cwolz-romberger@csumb.edu>
2013-03-16 08:11:51 -0700

    This is everything I had last night.

--- src/com/example/csumb_spa/MainActivity.java
+++ src/com/example/csumb_spa/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.csumb_spa;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.app.*;
+import android.content.*;
+import android.net.*;
+import android.widget.*;
+import android.view.*;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +17,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final Button button = (Button) findViewById(R.id.TestButton);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v){
+				setContentView(R.layout.activity_signs__main);
+			}
+		});
+		
 	}
 
 	@Override
@@ -17,6 +31,10 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void myClickHandler() {
+		setContentView(R.layout.activity_signs__main);
 	}
 
 }
